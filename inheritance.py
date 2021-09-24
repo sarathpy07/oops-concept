eab04feae9251631563806da93c0ce878a84e9ba
sarathpy07 <89681045+sarathpy07@users.noreply.github.com>
2021-09-24 17:58:55 +0530

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,12 @@
+class father:
+    def parent(self):
+        print( "father name is dad and age is 50")
+class son (father):
+    def child(self):
+        print("son name is sara")
+obj=son()
+obj.parent()
+obj.child()
+#creating one obj for son()it also used for parent
+
+    
